f4c48cfac1fce5540deaca793ce3f5ef1bb96368
August Guidry <august@augustguidry.com>
2024-07-25 13:56:56 -0500

    Spellcheck + linebreak to markdown files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,3 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd([[colorscheme solarized-osaka]])
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  command = "setlocal linebreak spell",
+})
